b07039ef6c6cecca5e592e2d3d5c83b151d5446c
shihabcse1 <tomdshihab@gmail.com>
2022-11-07 22:12:56 +0600

    handler._user.get method added to get data of an  user

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -1,6 +1,7 @@
 // dependencies
 const data = require("../../lib/data");
 const { hash } = require("../../helpers/utilities");
+const { parseJSON } = require("../../helpers/utilities");
 
 // module scaffolding
 const handler = {};
@@ -77,6 +78,34 @@ handler._users.post = (requestProperties, callback) => {
     } else {
         callback(400, {
             error: "You have a problem in your request",
+        });
+    }
+};
+
+// TODO: Authentication
+handler._users.get = (requestProperties, callback) => {
+    // check the phone number if valid
+    const phone =
+        typeof requestProperties.queryStringObject.phone === "string" &&
+        requestProperties.queryStringObject.phone.trim().length === 11
+            ? requestProperties.queryStringObject.phone
+            : false;
+    if (phone) {
+        // lookup the user
+        data.read("users", phone, (err, u) => {
+            const user = { ...parseJSON(u) };
+            if (!err && user) {
+                delete user.password;
+                callback(200, user);
+            } else {
+                callback(404, {
+                    error: "Requested user was not found!",
+                });
+            }
+        });
+    } else {
+        callback(404, {
+            error: "Requested user was not found!",
         });
     }
 };

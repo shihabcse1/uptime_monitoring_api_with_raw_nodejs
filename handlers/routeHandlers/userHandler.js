8f8cc12fc70a942da301899fa85359adc14dfdc4
shihabcse1 <tomdshihab@gmail.com>
2022-11-08 11:04:08 +0600

    handler._user.put method added to update information of an  user

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -110,4 +110,79 @@ handler._users.get = (requestProperties, callback) => {
     }
 };
 
+// @TODO: Authentication
+handler._users.put = (requestProperties, callback) => {
+    // check the phone number if valid
+
+    const phone =
+        typeof requestProperties.body.phone === "string" &&
+        requestProperties.body.phone.trim().length === 11
+            ? requestProperties.body.phone
+            : false;
+
+    const firstName =
+        typeof requestProperties.body.firstName === "string" &&
+        requestProperties.body.firstName.trim().length > 0
+            ? requestProperties.body.firstName
+            : false;
+
+    const lastName =
+        typeof requestProperties.body.lastName === "string" &&
+        requestProperties.body.lastName.trim().length > 0
+            ? requestProperties.body.lastName
+            : false;
+
+    const password =
+        typeof requestProperties.body.password === "string" &&
+        requestProperties.body.password.trim().length > 0
+            ? requestProperties.body.password
+            : false;
+
+    if (phone) {
+        if (firstName || lastName || password) {
+            // lookup the user
+            data.read("users", phone, (err1, uData) => {
+                const userData = { ...parseJSON(uData) };
+
+                if (!err1 && userData) {
+                    if (firstName) {
+                        userData.firstName = firstName;
+                    }
+                    if (lastName) {
+                        userData.lastName = lastName;
+                    }
+                    if (password) {
+                        userData.password = hash(password);
+                    }
+
+                    // store to database
+                    data.update("users", phone, userData, (err2) => {
+                        if (!err2) {
+                            callback(200, {
+                                message: "User was updated successfully!",
+                            });
+                        } else {
+                            callback(500, {
+                                error: "There was a problem in the server side!",
+                            });
+                        }
+                    });
+                } else {
+                    callback(400, {
+                        error: "You have a problem in your request!",
+                    });
+                }
+            });
+        } else {
+            callback(400, {
+                error: "You have a problem in your request!",
+            });
+        }
+    } else {
+        callback(400, {
+            error: "Invalid phone number. Please try again!",
+        });
+    }
+};
+
 module.exports = handler;
